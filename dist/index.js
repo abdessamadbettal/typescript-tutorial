67f6b9e3737dc15e5d78458f62a010a79900dfca
abdessamad bettal <abdessamadbattal@gmail.com>
2023-08-20 22:56:09 +0100

    add generics and subclasses

--- dist/index.js
+++ dist/index.js
@@ -60,5 +60,31 @@ class Person {
         this.id = id;
         this.name = name;
     }
+    register() {
+        return `${this.name} is now registered`;
+    }
 }
-const brad = new Person(1, 'Abdessamad bettal');
+const Abdo = new Person(1, 'Abdessamad bettal');
+const John = new Person(2, 'John Doe');
+// Subclasses
+class Employee extends Person {
+    constructor(id, name, position) {
+        super(id, name); // super is used to call the constructor of the parent class
+        this.position = position;
+    }
+}
+const emp = new Employee(3, 'Shawn', 'Developer');
+// Generics
+function getArray(items) {
+    return new Array().concat(items); // this function takes an array of type T and return an array of type T
+}
+let numArray = getArray([1, 2, 3, 4]); // this means that numArray is an array of numbers
+let strArray = getArray(['brad', 'John', 'Jill']); // this means that strArray is an array of strings
+numArray.push(5);
+strArray.push('Abdo');
+// numArray.push('hello'); // this will give an error because numArray is an array of numbers
+// strArray.push(5); // this will give an error because strArray is an array of strings
+// numArray.push(true); // this will give an error because numArray is an array of numbers
+// strArray.push(true); // this will give an error because strArray is an array of strings
+// numArray.push({}); // this will give an error because numArray is an array of numbers
+// strArray.push({}); // this will give an error because strArray is an array of strings
--- src/index.ts
+++ src/index.ts
@@ -82,8 +82,15 @@ interface MathFunc {
 const add: MathFunc = (x: number, y: number): number => x + y; // this means that add is a function that takes two numbers and return a number
 const sub: MathFunc = (x: number, y: number): number => x - y; // this means that sub is a function that takes two numbers and return a number
 
+
+interface PersonInterface {
+    id: number,
+    name: string,   
+    register(): string, // this means that register is a function that takes no parameters and return a string
+}
+
 // Classes
-class Person {
+class Person implements PersonInterface {
     id: number;
     name: string;
 
@@ -91,9 +98,50 @@ class Person {
         this.id = id;
         this.name = name;
     }
+
+    register() {
+        return `${this.name} is now registered`;
+    }
 }
 
-const brad = new Person(1, 'Abdessamad bettal');
+const Abdo = new Person(1, 'Abdessamad bettal');
+const John = new Person(2, 'John Doe');
+
+// Subclasses
+class Employee extends Person {
+    position: string;
+
+    constructor(id: number, name: string, position: string) {
+        super(id, name); // super is used to call the constructor of the parent class
+        this.position = position;
+    }
+}
+
+const emp = new Employee(3, 'Shawn', 'Developer');
+
+// Generics
+function getArray<T>(items: T[]): T[] { // T is a generic type that can be any type 
+    return new Array().concat(items);  // this function takes an array of type T and return an array of type T
+}
+
+let numArray = getArray<number>([1, 2, 3, 4]); // this means that numArray is an array of numbers
+let strArray = getArray<string>(['brad', 'John', 'Jill']); // this means that strArray is an array of strings
+
+numArray.push(5);
+strArray.push('Abdo');
+
+// numArray.push('hello'); // this will give an error because numArray is an array of numbers
+// strArray.push(5); // this will give an error because strArray is an array of strings
+
+// numArray.push(true); // this will give an error because numArray is an array of numbers
+// strArray.push(true); // this will give an error because strArray is an array of strings
+
+// numArray.push({}); // this will give an error because numArray is an array of numbers
+// strArray.push({}); // this will give an error because strArray is an array of strings
+
+
+
+
 
 
 
